0bf1cad6dba7a3c1ccc68e9ba7f871dcb0393f74
Saywanbeshooni <Saywanbeshooni2@gmail.com>
2024-05-19 11:22:27 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function  blackG()
+{
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+}
+function  whiteG()
+{
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+}
